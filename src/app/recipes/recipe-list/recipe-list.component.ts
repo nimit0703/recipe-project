a74d323d387d4de3db7b432070f87ec4ad781a1d
nimit0703 <nimit.190410107143@gmail.com>
2023-09-02 00:05:23 +0530

    show recipe-detail component when recipe selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent  implements OnInit{
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes : Recipe[] = [
     new Recipe(
       'a test recipe', 'test recipe','https://images.unsplash.com/photo-1495521821757-a1efb6729352?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1926&q=80'),
@@ -14,7 +17,15 @@ export class RecipeListComponent  implements OnInit{
       'a test recipe', 'test recipe','https://images.unsplash.com/photo-1495521821757-a1efb6729352?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1926&q=80')
 
   ];
-  ngOnInit(): void {
-    // throw new Error('Method not implemented.');
+
+  onRecipeSelected(recipe :Recipe){
+    // console.log("OnRecipeSelected works in Recipe list",recipe)
+    this.recipeWasSelected.emit(recipe);
+  }
+  ngOnInit(): void {   
   }
 }
+function output(): (target: RecipeListComponent, propertyKey: "recipeWasSelected") => void {
+  throw new Error('Function not implemented.');
+}
+
